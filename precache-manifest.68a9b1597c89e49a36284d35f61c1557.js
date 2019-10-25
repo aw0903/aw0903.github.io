bf7c30cfe61674533ae36763647c4ba7f867e100
aw0903 <annawang8903@gmail.com>
2019-10-25 16:49:43 +0800

    run build

---
+++ precache-manifest.68a9b1597c89e49a36284d35f61c1557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90981f0263c66b2f836374d8342111ec",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "99057b7b80f4337a47e1",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "ddace62cba0d09e07865",
+    "url": "/bmi-calculator/static/css/main.e8f52ae8.chunk.css"
+  },
+  {
+    "revision": "99057b7b80f4337a47e1",
+    "url": "/bmi-calculator/static/js/2.0c9b15aa.chunk.js"
+  },
+  {
+    "revision": "ddace62cba0d09e07865",
+    "url": "/bmi-calculator/static/js/main.927c7803.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
